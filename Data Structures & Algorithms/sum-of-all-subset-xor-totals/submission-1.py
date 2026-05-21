0b7d30e9314113eb208c4bacc851ba4cc96c9f11
Divakaran A <divakaranoffical@gmail.com>
2026-05-21 14:54:16 +0530

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = res|n
+
+        return res * 2 ** (len(nums) - 1) 
+
+        
